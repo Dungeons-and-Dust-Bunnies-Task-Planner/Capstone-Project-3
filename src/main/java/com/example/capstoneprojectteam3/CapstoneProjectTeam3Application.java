51d1100d17f980a8d4ef382140423e96f4ad49fa
Dawson Schroeder <schroeder.dawson.d@gmail.com>
2023-06-06 12:05:50 -0500

    init

---
+++ src/main/java/com/example/capstoneprojectteam3/CapstoneProjectTeam3Application.java
@@ -0,0 +1,13 @@
+package com.example.capstoneprojectteam3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneProjectTeam3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapstoneProjectTeam3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/capstoneprojectteam3/CapstoneProjectTeam3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.capstoneprojectteam3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneProjectTeam3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
