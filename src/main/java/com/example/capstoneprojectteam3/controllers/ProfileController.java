8596f6499c4cd889c9ea3895e88d629c75340fc1
Lennox Ballard <noellemb@gmail.com>
2023-06-08 10:34:40 -0500

    profile start

---
+++ src/main/java/com/example/capstoneprojectteam3/controllers/ProfileController.java
@@ -0,0 +1,14 @@
+package com.example.capstoneprojectteam3.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProfileController {
+
+    @GetMapping("/profile")
+    public String showProfile() {
+
+        return "/profile";
+    }
+}
