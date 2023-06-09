45b6f5189691fef82d3cc9f5131e1137ad70f427
Jared Orler <orlerdev@icloud.com>
2023-06-09 11:30:59 -0500

    task input modal, overlay, and form created with styling

--- src/main/java/com/example/capstoneprojectteam3/config/SecurityConfig.java
+++ src/main/java/com/example/capstoneprojectteam3/config/SecurityConfig.java
@@ -10,31 +10,27 @@ import org.springframework.security.web.SecurityFilterChain;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-public class SecurityConfig {
+public class SecurityConfig{
 
-    @Bean
-    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests
-                .requestMatchers("/tasks/create", "/tasks/*/edit", "/profile", "/battlegrounds").authenticated()
-                .requestMatchers("/tasks", "/templates/**", "/register", "/login", "/home", "/").permitAll()
-                .requestMatchers("/css/**", "/js/**", "/images/**").permitAll()
-        )
-                .formLogin((login) -> login.loginPage("/login").defaultSuccessUrl("/home"))
-                .logout((logout) -> logout.logoutSuccessUrl("/home"))
-                .httpBasic(withDefaults());
-        return http.build();
+	@Bean
+	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception{
+//        http.authorizeHttpRequests((requests) -> requests
+//                .requestMatchers("/tasks/create", "/tasks/*/edit", "/profile", "/battlegrounds").authenticated()
+//                .requestMatchers("/tasks", "/templates/**", "/register", "/login", "/home", "/").permitAll()
+//                .requestMatchers("/css/**", "/js/**", "/images/**").permitAll()
+		http.authorizeHttpRequests((requests) -> requests
+						.anyRequest().permitAll()
+				)
+				.formLogin((login) -> login.loginPage("/login").defaultSuccessUrl("/home"))
+				.logout((logout) -> logout.logoutSuccessUrl("/home"))
+				.httpBasic(withDefaults());
+		return http.build();
+	}
 
-
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
-
-  //  http.authorizeHttpRequests((requests) -> requests
-//                .anyRequest().permitAll()
-//        );
+	@Bean
+	public PasswordEncoder passwordEncoder(){
+		return new BCryptPasswordEncoder();
+	}
 
 
 }
