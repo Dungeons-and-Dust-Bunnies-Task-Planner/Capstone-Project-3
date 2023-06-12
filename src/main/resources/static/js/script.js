3f0bbea06c8cebea020674df50866e9ebda7c60d
Jared Orler <orlerdev@icloud.com>
2023-06-12 16:28:19 -0500

    functional task highlighting, and selection added

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,49 +1,72 @@
-const battleIcon = document.querySelector('.battle-icon')
-const battleSlideOut = document.querySelector('.battle-slide-out')
-const battleCheckbox = document.querySelector('.battle-checkbox')
-const battleText = document.querySelector('.battle-text')
-const addBattleWrap = document.querySelector('.add-battle-wrap')
-const addBattleInput = document.querySelector('.add-battle-input')
-const addBattleBtn = document.querySelector('.add-battle-btn')
-const taskModal = document.querySelector('.task-modal');
-const modalOverlay = document.querySelector('.modal-overlay');
-const openTaskModal = document.querySelector('.new-task-wrap');
-const logOutForm = document.getElementById("logOutForm");
-const logOutButton = document.getElementById("logOutButton");
+(() => {
 
+		const battleIcon = document.querySelector('.battle-icon')
+		const battleSlideOut = document.querySelector('.battle-slide-out')
+		// const battlesList = document.querySelector('.battles-list')
+		// const battleCheckbox = document.querySelector('.battle-checkbox')
+		// const battleText = document.querySelector('.battle-text')
+		const addBattleWrap = document.querySelector('.add-battle-wrap')
+		const addBattleInput = document.querySelector('.add-battle-input')
+		const addBattleBtn = document.querySelector('.add-battle-btn')
+		const taskModal = document.querySelector('.task-modal')
+		const battleTasks = document.querySelector('.tasks-list')
+		const task = document.querySelectorAll('.task')
+		const battle = document.querySelector('.battle')
+		const modalOverlay = document.querySelector('.modal-overlay')
+		const openTaskModal = document.querySelector('.new-task-wrap')
+		const logOutForm = document.querySelector('.logout-form')
+		const logOutBtn = document.querySelector('.logout-btn')
+
+
+/////////////////////
+// FUNCTIONS
+/////////////////////
+		const checkForClassValue = (element, className) => {
+				return element.classList.contains(className)
+		}
 
 /////////////////////
 // EVENT LISTENERS
 /////////////////////
 
 // BATTLE ICON
-battleIcon.addEventListener('click', e => {
-    e.preventDefault()
-    battleSlideOut.classList.toggle('open')
-})
-// BATTLE CHECKBOX
-battleCheckbox.addEventListener('click', e => {
-    e.preventDefault()
-    battleText.style.textDecoration === 'line-through' ? (battleText.style.textDecoration = 'none') && (battleCheckbox.style.background = 'var(--primary)') : (battleText.style.textDecoration = 'line-through') && (battleCheckbox.style.background = '#000')
-    battleCheckbox.style.background === 'var(--primary)' ? battleCheckbox.style.boxShadow = 'inset 0 0 8px #000' : battleCheckbox.style.boxShadow = 'inset 0 0 8px var(--primary)'
-    console.log('fired')
-})
+		battleIcon.addEventListener('click', e => {
+				e.preventDefault()
+				battleSlideOut.classList.toggle('open')
+		})
+
 // ADD BATTLE BUTTON
-addBattleBtn.addEventListener('click', () => {
-    addBattleWrap.classList.toggle('active')
-    addBattleInput.focus()
-})
+		addBattleBtn.addEventListener('click', () => {
+				addBattleWrap.classList.toggle('active')
+				addBattleInput.focus()
+		})
 // OPEN TASK MODAL
-openTaskModal.addEventListener('click', e => {
-    modalOverlay.classList.toggle('hidden');
-    taskModal.classList.toggle('hidden');
-})
+		openTaskModal.addEventListener('click', e => {
+				modalOverlay.classList.toggle('hidden')
+				taskModal.classList.toggle('hidden')
+		})
 // MODAL OVERLAY CLICK EVENT
-modalOverlay.addEventListener('click', e => {
-    modalOverlay.classList.toggle('hidden');
-    taskModal.classList.toggle('hidden')
-})
+		modalOverlay.addEventListener('click', e => {
+				modalOverlay.classList.toggle('hidden')
+				taskModal.classList.toggle('hidden')
+		})
 // LOGOUT BUTTON
-logOutButton.addEventListener('click', () => {
-    logOutForm.submit();
-});
+		logOutBtn.addEventListener('click', () => {
+				logOutForm.submit()
+		})
+// DYNAMIC TASKS
+		document.addEventListener('DOMContentLoaded', () => {
+				console.log('Battlegrounds page loaded')
+				document.querySelectorAll('.task').forEach(el => {
+						el.addEventListener('click', () => {
+								el.classList.toggle('complete')
+								el.classList.toggle('not-complete')
+						})
+				})
+		})
+})()
+
+
+
+
+
