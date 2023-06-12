604ac249baa158bc6d58c84a82d087a1f1d21e6f
Jared Orler <orlerdev@icloud.com>
2023-06-12 10:06:28 -0500

    comments added to battlegrounds.html and script.js

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -8,38 +8,42 @@ const addBattleBtn = document.querySelector('.add-battle-btn')
 const taskModal = document.querySelector('.task-modal');
 const modalOverlay = document.querySelector('.modal-overlay');
 const openTaskModal = document.querySelector('.new-task-wrap');
+const logOutForm = document.getElementById("logOutForm");
+const logOutButton = document.getElementById("logOutButton");
 
+
+/////////////////////
+// EVENT LISTENERS
+/////////////////////
+
+// BATTLE ICON
 battleIcon.addEventListener('click', e => {
     e.preventDefault()
     battleSlideOut.classList.toggle('open')
 })
-
+// BATTLE CHECKBOX
 battleCheckbox.addEventListener('click', e => {
     e.preventDefault()
     battleText.style.textDecoration === 'line-through' ? (battleText.style.textDecoration = 'none') && (battleCheckbox.style.background = 'var(--primary)') : (battleText.style.textDecoration = 'line-through') && (battleCheckbox.style.background = '#000')
     battleCheckbox.style.background === 'var(--primary)' ? battleCheckbox.style.boxShadow = 'inset 0 0 8px #000' : battleCheckbox.style.boxShadow = 'inset 0 0 8px var(--primary)'
     console.log('fired')
 })
-
+// ADD BATTLE BUTTON
 addBattleBtn.addEventListener('click', () => {
     addBattleWrap.classList.toggle('active')
     addBattleInput.focus()
 })
-
+// OPEN TASK MODAL
 openTaskModal.addEventListener('click', e => {
     modalOverlay.classList.toggle('hidden');
     taskModal.classList.toggle('hidden');
 })
-
+// MODAL OVERLAY CLICK EVENT
 modalOverlay.addEventListener('click', e => {
     modalOverlay.classList.toggle('hidden');
     taskModal.classList.toggle('hidden')
 })
-
-
-
-const logOutForm = document.getElementById("logOutForm");
-const logOutButton = document.getElementById("logOutButton");
+// LOGOUT BUTTON
 logOutButton.addEventListener('click', () => {
     logOutForm.submit();
 });
