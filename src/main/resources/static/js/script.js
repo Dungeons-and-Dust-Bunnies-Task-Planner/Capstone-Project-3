5e90559a519ff054638d77f171d703038ab9eeec
Jared Orler <orlerdev@icloud.com>
2023-06-13 11:50:54 -0500

    tasks are only shown for active battles

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -2,16 +2,10 @@
 
 		const battleIcon = document.querySelector('.battle-icon')
 		const battleSlideOut = document.querySelector('.battle-slide-out')
-		// const battlesList = document.querySelector('.battles-list')
-		// const battleCheckbox = document.querySelector('.battle-checkbox')
-		// const battleText = document.querySelector('.battle-text')
 		const addBattleWrap = document.querySelector('.add-battle-wrap')
 		const addBattleInput = document.querySelector('.add-battle-input')
 		const addBattleBtn = document.querySelector('.add-battle-btn')
 		const taskModal = document.querySelector('.task-modal')
-		const battleTasks = document.querySelector('.tasks-list')
-		const task = document.querySelectorAll('.task')
-		const battle = document.querySelector('.battle')
 		const openTaskModal = document.querySelector('.new-task-wrap')
 		const modalOverlay = document.querySelector('.modal-overlay')
 		const slideOutOverlay = document.querySelector('.slide-out-overlay')
@@ -65,23 +59,61 @@
 // DYNAMIC TASKS
 		document.addEventListener('DOMContentLoaded', () => {
 				console.log('Battlegrounds page loaded')
-				document.querySelectorAll('.battle').forEach(function(battleElement) {
-						battleElement.addEventListener('click', function() {
+				document.querySelectorAll('.battle').forEach(function (battleElement, idx){
+						battleElement.addEventListener('click', function (e) {
 								battleElement.classList.toggle('active')
 								battleElement.classList.toggle('inactive')
+								console.log('battle class toggled')
 
+								const battles = document.querySelectorAll('.battle')
+								const battleTasks = document.querySelectorAll('.battle-tasks')
+
+								if (battles[idx].classList.contains('active')) {
+										battleTasks[idx].classList.remove('hidden')
+								}
+								if (battles[idx].classList.contains('inactive')) {
+										battleTasks[idx].classList.add('hidden')
+								}
 						})
+
+						//
+						// battleElement.addEventListener('click', function () {
+						// 		console.log('second click event fired')
+						// 		const battleTasks = document.querySelector('.battle-tasks')
+						// 		if (battleElement.classList.contains('active')) {
+						// 				battleTasks.classList.remove('hidden')
+						// 				console.log('hidden class removed')
+						// 		}
+						// 		if (battleElement.classList.contains('inactive')) {
+						// 				battleTasks.classList.add('hidden')
+						// 				console.log('hidden class added')
+						// 		}
+						// })
 				})
-				document.querySelectorAll('.task').forEach(el => {
-						el.addEventListener('click', () => {
-								el.classList.toggle('complete')
-								el.classList.toggle('not-complete')
+
+				document.querySelectorAll('.task').forEach(taskElement => {
+						taskElement.addEventListener('click', () => {
+								console.log('task click fired')
+								taskElement.classList.toggle('complete')
+								taskElement.classList.toggle('not-complete')
+								console.log('task class changed')
 						})
 				})
 		})
 })()
 
 
+// const battles = document.querySelectorAll('.battle')
+// const battleTasks = document.querySelectorAll('.battle-tasks')
+// for (let i = 0; i <= battles.length; i++) {
+// 		for (let j = 0; i <= battleTasks.length; j++) {
+// 				if (battles[i].classList.contains('active')) {
+// 						battleTasks[j].classList.remove('hidden')
+// 				} else {
+// 						battleTasks[j].classList.add('hidden')
+// 				}
+// 		}
+// }
 
 
 
