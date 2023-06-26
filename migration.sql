bf2dfbf80d44de42d156107bc66bbb4b58983ba1
Lennox Ballard <noellemb@gmail.com>
2023-06-26 14:54:13 -0500

    update images for badges

--- migration.sql
+++ migration.sql
@@ -4,21 +4,24 @@ use dungeons_and_dust_bunnies_db;
 # BEFORE RUNNING THE REST OF THIS----------------------------------------------------
 # YOU HAVE TO HAVE AT LEAST 1 USER IN THE DB, THIS IS SET UP TO WORK WITH user_id = 1
 
-INSERT INTO badges (badge_title, badge_body)
-VALUES ('First blood', 'Completed your first battle!'),
-       ('Novice do-er', 'Completed 3 battles'),
-       ('Regular getter-doner', 'Completed 5 battles'),
-       ('Cosmic Cleaner', 'Defeated the cosmic hat octopuss of filth');
+INSERT INTO badges (badge_image,badge_title, badge_body)
+VALUES ('https://cdn.discordapp.com/attachments/415980182021603329/1122909077878603858/IMG_1856.png', 'First blood', 'Completed your first battle!'),
+       ('https://cdn.discordapp.com/attachments/415980182021603329/1122909077622767646/IMG_1857.png', 'Novice do-er', 'Completed 3 battles'),
+       ('https://cdn.discordapp.com/attachments/415980182021603329/1122909077304004699/IMG_1858.png', 'Regular getter-doner', 'Completed 5 battles'),
+       ('https://cdn.discordapp.com/attachments/415980182021603329/1122909075919863898/IMG_1859.png', 'Cosmic Cleaner', 'Defeated the cosmic hat octopuss of filth');
+
+
 
 INSERT INTO monsters (name)
 VALUES ('Blobbus McSlimey'),
        ('Dust Bunny Prime'),
        ('OctoRizz the Slippery'),
-       ('Sir OwlBear the Unclean');
+       ('Sir OwlBear the Unclean'),
+       ('The Cosmic Hat Octopuss of Filth');
 
 INSERT INTO monster_images (monster_img, stage, monster_img_id)
 VALUES
-        ('https://cdn.filestackcontent.com/ohsq2CNgSlgZidDS2wEI', 1,1 ),
+        ('https://cdn.filestackcontent.com/ohsq2CNgSlgZidDS2wEI',1,1),
         ('https://cdn.filestackcontent.com/ytGYW57OTaOssPKoUfxi',2,1),
         ('https://cdn.filestackcontent.com/OZNx5tqOQfijYoeuSND6',3,1),
         ('https://cdn.filestackcontent.com/K6HxhqrATpOjR90fRrc8',4,1),
@@ -36,7 +39,12 @@ VALUES
         ('https://cdn.filestackcontent.com/ETNraeT7QJmlkZq7STYW',2,4),
         ('https://cdn.filestackcontent.com/HaccfppSQj25FYEEqpH7',3,4),
         ('https://cdn.filestackcontent.com/ydXl8EWVS2y4M1EJbqK5',4,4),
-        ('https://cdn.filestackcontent.com/VLOjOCFRyuZtOro7luaw',5,4);
+        ('https://cdn.filestackcontent.com/VLOjOCFRyuZtOro7luaw',5,4),
+        ('https://cdn.discordapp.com/attachments/415980182021603329/1119086923118559272/dishes.png', 5,1),
+        ('https://cdn.discordapp.com/attachments/415980182021603329/1119086924024528926/dishes3.png',5,2),
+        ('https://cdn.discordapp.com/attachments/415980182021603329/1119086924271980576/dishes4.png',5,3),
+        ('https://cdn.discordapp.com/attachments/415980182021603329/1119086924553007215/dishes5.png',5,4),
+        ('https://cdn.discordapp.com/attachments/415980182021603329/1119086924838223883/dishes6.png',5,5);
 
 INSERT INTO battles (monster_id, user_id, title, status)
 VALUES (1, 1, 'Battle name 1 placholder', 0),
