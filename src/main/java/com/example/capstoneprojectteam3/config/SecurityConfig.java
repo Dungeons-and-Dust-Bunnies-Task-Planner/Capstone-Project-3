0fc41d335eaf6e90da4dd67996564d58c586e0af
Ezra M <124730310+Ez-Megalodon@users.noreply.github.com>
2023-06-28 18:02:55 -0500

    health-bar, monster stages working.

--- src/main/java/com/example/capstoneprojectteam3/config/SecurityConfig.java
+++ src/main/java/com/example/capstoneprojectteam3/config/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig {
         return http.authorizeHttpRequests((authorize) -> authorize
 
 //    -------------------UNCOMMENT THIS CODE FOR SECURED ACCESS ---------------------------
-                        .requestMatchers("/profile","/profile/**", "/battlegrounds", "/battlegrounds/**", "/battleList","/claim/badge","/edit/profile").authenticated()
+                        .requestMatchers("/profile","/profile/**", "/battlegrounds", "/battlegrounds/**","/battlegrounds/complete-task", "/battleList","/claim/badge","/edit/profile").authenticated()
 
                         .requestMatchers( "/register", "/registration", "/login", "/home", "/", "/about","/contact", "/send-email", "/welcome", "/monsterList").permitAll()
                         .requestMatchers("/css/**", "/js/**", "/images/**", "/keys.js").permitAll()
