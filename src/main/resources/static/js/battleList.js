07179afe000e64e3f267abe6cd218e829c93d0d8
Ezra M <124730310+Ez-Megalodon@users.noreply.github.com>
2023-06-29 17:01:39 -0500

    updated battleground media queries and battleList queries

--- src/main/resources/static/js/battleList.js
+++ src/main/resources/static/js/battleList.js
@@ -1,8 +1,11 @@
 const editButtons = document.querySelectorAll('.editBattle');
 const editModals = document.querySelectorAll('.editModal');
 const editModalOverlays = document.querySelectorAll('.edit-modal-overlay');
-const blur = document.getElementById('battleDiv');
-const editModalOverlay = document.querySelector('.edit-modal-overlay');
+// const blur = document.getElementById('battleDiv');
+// const editModalOverlay = document.querySelector('.edit-modal-overlay');
+const deleteModals = document.querySelectorAll('.delete-confirm-modal');
+const cancelDeleteBtns = document.querySelectorAll('#cancel-delete');
+const openDeleteBtns = document.querySelectorAll('#delete-confirm-open-btn');
 
 editButtons.forEach((editButton, index) => {
     editButton.addEventListener('click', () => {
@@ -15,5 +18,21 @@ editModalOverlays.forEach((modalOverlay, index) => {
     modalOverlay.addEventListener('click', () => {
         editModalOverlays[index].classList.toggle('hidden');
         editModals[index].classList.toggle('hidden');
+        if (!deleteModals[index].classList.contains('hidden')){
+            deleteModals[index].classList.toggle('hidden');
+        }
     });
 });
+
+openDeleteBtns.forEach((btn, i) => {
+    btn.addEventListener('click', () => {
+        deleteModals[i].classList.toggle('hidden');
+    })
+});
+
+cancelDeleteBtns.forEach((btn, i) => {
+    btn.addEventListener('click', () => {
+        deleteModals[i].classList.toggle('hidden');
+    })
+});
+
